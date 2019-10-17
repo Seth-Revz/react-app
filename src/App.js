8f5986de27148c1f9c10da3c4793aa79702c34ca
Seth <sethmorris.dev@gmail.com>
2019-10-16 22:07:35 -0400

    Added shuriken

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ function App() {
       <link href="https://fonts.googleapis.com/css?family=Racing+Sans+One&display=swap" rel="stylesheet"></link>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
         <header className="App-header">
-          <div className="rectangle">
-            <h3 id="name">
-              Revz
-            </h3>
-            <p>
-              - <a href="http://steamcommunity.com/id/smrevz/" target="_blank" rel="noopener noreferrer">Steam</a>
-            </p>
-            <p>
-              - <a href="https://discord.gg/SZk6qjp" target="_blank" rel="noopener noreferrer">Discord</a>
-            </p>
-            <div className="bg-animation overlay">
-          <div id="stars3"></div>
+          <div className="info">
+            <div className="shuriken"></div>
+            <div className="text">
+              <h3 id="text">
+                Revz
+              </h3>
+              <p>
+                <a href="http://steamcommunity.com/id/smrevz/" target="_blank" rel="noopener noreferrer">Steam</a>
+              </p>
+              <p>
+                <a href="https://discord.gg/SZk6qjp" target="_blank" rel="noopener noreferrer">Discord</a>
+              </p>
+            </div>
+          </div>
+          <div className="bg-animation overlay">
+            <div id="stars3"></div>
             <div id="stars4"></div>
             <div id="stars2"></div>
-            </div>
           </div>
         </header>
     </div>
